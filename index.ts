9b8dca18ca19c60ddec6ce3884e31413c7bf20f7
Kanjam Bhat Lidhoo <Kanjambhat.Lidhoo@pens.com>
2024-03-23 23:48:13 +0530

    Removed index.ts file. Instead will upload the index.js file later

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { App } from "./App";
-
-const app = new App();
-
-const server = app.app;
-
-server.listen(3000, () => {
-    console.log("The application has been started and is listening on port 3000.");
-});
